e7c2f18037e7a9bc6d5688fce2274dba5e6ee970
Gokul <gokul.gk96@gmail.com>
2021-11-27 16:33:18 +0530

    Mainstream Completed

--- Tic_tac_toe_game/ViewController.swift
+++ Tic_tac_toe_game/ViewController.swift
@@ -13,9 +13,10 @@ let zero = "O"
 
 class ViewController: UIViewController {
  
+    @IBOutlet weak var Turn_label: UILabel!
     
     var inital = zero
-    var count = 0
+ 
     
     var button_array = [UIButton]()
 
@@ -23,6 +24,7 @@ class ViewController: UIViewController {
     @IBOutlet weak var A2_button: UIButton!
     @IBOutlet weak var A3_button: UIButton!
     
+
     
     @IBOutlet weak var B1_button: UIButton!
     @IBOutlet weak var B2_button: UIButton!
@@ -44,11 +46,11 @@ class ViewController: UIViewController {
         button_array.append(C1_button)
         button_array.append(C2_button)
         button_array.append(C3_button)
+        
+        Turn_label.text = "X Turn"
     }
 
     @IBAction func Button_main_action(_ sender: UIButton) {
-        
-       
         
         if(sender.title(for: .normal) == nil )
         {
@@ -56,46 +58,132 @@ class ViewController: UIViewController {
             {
                 sender.setTitle("X", for: .normal)
                 inital = cross
+                Turn_label.text = "O Turn"
 
             }
             else if ( inital == "X")
             {
                 sender.setTitle("O", for: .normal)
                 inital = zero
+                Turn_label.text = "X Turn"
                
             }
-            
-           
         }
         
+        if(victory(cross))
+        {
+            
+            
+            let alert = UIAlertController(title: "Alert", message: "Match won by CROSS team", preferredStyle: UIAlertController.Style.alert)
+            alert.addAction(UIAlertAction(title: "Reset", style: UIAlertAction.Style.default, handler:
+                { action in
+                        
+                     for button in self.button_array
+                            {
+                                button.setTitle(nil, for: .normal)
+                            }
         
-        for button in button_array
+                                                
+                   }))
+            self.present(alert, animated: true, completion: nil)
+        }
+        
+        if(victory(zero))
         {
            
-            if(button.title(for: .normal) != nil)
-            {
-               count += 1
-            }
             
-            if(count == 9)
+            let alert = UIAlertController(title: "Alert", message: "Match won by ZERO team", preferredStyle: UIAlertController.Style.alert)
+            alert.addAction(UIAlertAction(title: "Reset", style: UIAlertAction.Style.default, handler:
+                { action in
+                        
+                     for button in self.button_array
+                            {
+                                button.setTitle(nil, for: .normal)
+                            }
+                                
+                 
+                   }))
+            self.present(alert, animated: true, completion: nil)
+        }
+    
+        
+        
+        if(draw())
+        {
+            let alert = UIAlertController(title: "Alert", message: "Match has been Draw", preferredStyle: UIAlertController.Style.alert)
+            alert.addAction(UIAlertAction(title: "Reset", style: UIAlertAction.Style.default, handler:
+                { action in
+                        
+                     for button in self.button_array
+                            {
+                                button.setTitle(nil, for: .normal)
+                            }
+                    
+                                                
+                   }))
+            self.present(alert, animated: true, completion: nil)
+        }
+             
+   }
+    
+    
+ func draw() -> Bool
+    {
+        for button in button_array
+        {
+            if(button.title(for: .normal) == nil)
             {
-                
-                let uialert = UIAlertController(title: "TIC TAC TOE", message: "Full", preferredStyle: UIAlertController.Style.alert)
-                      uialert.addAction(UIAlertAction(title: "Reset", style: UIAlertAction.Style.default, handler: { action in
-                        for button in self.button_array
-                        {
-                            button.setTitle(nil, for: .normal)
-                        }
-                    }))
-                   self.present(uialert, animated: true, completion: nil)
-                
-
-               
+                return false
             }
         }
+        return true
+    }
+
+    
+    func victory(_ s: String) -> Bool
+    {
+        if(A1_button.title(for: .normal) == s && A2_button.title(for: .normal) == s && A3_button.title(for: .normal) == s )
+        {
+            return true
+        }
+        if(B1_button.title(for: .normal) == s && B2_button.title(for: .normal) == s && B3_button.title(for: .normal) == s )
+        {
+            return true
+        }
+        if(C1_button.title(for: .normal) == s && C2_button.title(for: .normal) == s && C3_button.title(for: .normal) == s )
+        {
+            return true
+        }
         
-    count = 0
-   }
+        
+        if(A1_button.title(for: .normal) == s && B1_button.title(for: .normal) == s && C1_button.title(for: .normal) == s )
+        {
+            return true
+        }
+        if(A2_button.title(for: .normal) == s && B2_button.title(for: .normal) == s && C2_button.title(for: .normal) == s )
+        {
+            return true
+        }
+        if(A3_button.title(for: .normal) == s && B3_button.title(for: .normal) == s && C3_button.title(for: .normal) == s )
+        {
+            return true
+        }
+        
+        
+        
+        if(A1_button.title(for: .normal) == s && B2_button.title(for: .normal) == s && C3_button.title(for: .normal) == s )
+        {
+            return true
+        }
+        if(A3_button.title(for: .normal) == s && B2_button.title(for: .normal) == s && C1_button.title(for: .normal) == s )
+        {
+            return true
+        }
+    
+        
+        return false
+    }
+    
     
 }
 
